c6cf4d04531a6ecef9d4235d9f5b71743431a4dd
kosy-wer <protectorunmatched@gmai.com>
2024-08-23 09:17:58 +0700

    reaktoring code create order

--- Sistem-absensi/app/Http/Controllers/OrderController.php
+++ Sistem-absensi/app/Http/Controllers/OrderController.php
@@ -3,38 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreOrderRequest;
-use App\Http\Requests\CreateOrderRequest;
 use App\Http\Resources\OrderResource;
-use App\Http\Resources\PostResource;
-use Illuminate\Support\Facades\Validator;
 use App\Models\Order;
-use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-
-    public function store(Request $request)
+    public function store(StoreOrderRequest $request)
     {
-        //define validation rules
-        $validator = Validator::make($request->all(), [
-            'name'     => 'required',
-            'reason'   => 'required',
-            'status'   => 'required',
-        ]);
-
-        //check if validation fails
-        //
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
-
         $order = Order::create([
             'name'     => $request->name,
             'reason'   => $request->reason,
             'status'   => $request->status,
         ]);
 
-        //return response
-     return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $order);
+        return new OrderResource(true, 'Data Order Berhasil Ditambahkan!', $order);
     }
 }
+
--- Sistem-absensi/app/Http/Resources/OrderResource.php
+++ Sistem-absensi/app/Http/Resources/OrderResource.php
@@ -6,12 +6,37 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource
 {
-    public function toArray($request)
+    private $success;
+    private $message;
+
+    /**
+     * __construct
+     *
+     * @param  mixed $status                                         * @param  mixed $message
+     * @param  mixed $resource
+     * @return void
+     */
+
+    public function __construct($success, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->success = $success;
+        $this->message = $message;
+    }
+
+    /**
+     * toArray
+     *
+     * @param  mixed $request
+     * @return array
+     */
+
+    public function toArray($request): array
     {
         return [
-            'success'   => $this->status,
-            'message'   => $this->message,
-            'data'      => $this->resource,
+            'success' => $this->success,
+            'message' => $this->message,
+            'data'    => $this->resource,
         ];
     }
 }
